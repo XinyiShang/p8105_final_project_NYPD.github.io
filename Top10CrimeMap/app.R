e454a9cde4990fa13599af08ad02ade174fc230f
allisonzxia <zx2482@cumc.columbia.edu>
2023-12-08 09:19:40 -0500

    update shiny map

--- Top10CrimeMap/app.R
+++ Top10CrimeMap/app.R
@@ -1,16 +1,28 @@
+library(tidyverse)
+library(dplyr)
+library(lubridate)
+library(htmltools)
+library(viridis)
+library(shiny)
+library(plotly)
+library(shinydashboard)
+library(leaflet)
+library(ggmap)
+library(leaflet.extras)
 
 
+df_summary = read_csv("https://www.dropbox.com/scl/fi/hd8ugc0cq1k243ehvq2cu/df_summary.csv?rlkey=miirkjpx50l75xoc6iyom4ms3&dl=1")
+df_crime_patterns = read_csv("https://www.dropbox.com/scl/fi/9ecsdf8ps1gpx4hn3ox5o/df_crime_patterns_sample.csv?rlkey=ln5qd4mrxo690vd0dn6o0izjn&dl=1")
+crime_counts = read_csv("https://www.dropbox.com/scl/fi/uc7loy417kmshq5gulxme/crime_counts.csv?rlkey=okb1xv5qgt0lgxzioq5ha6kfz&dl=1")
 
-# Function to create a plot
+# Function to create linegraph
 create_plot <- function(ofnsDesc) {
   # Generate the plot title
   plot_title <- paste("Number of", ofnsDesc, "in Each Year")
   
   # Create the plot
-  df_crime_patterns |>
+  crime_counts |>
     filter(ofns_desc == ofnsDesc) |>
-    group_by(year) |>
-    summarize(count = n()) |>
     plot_ly(x = ~year, y = ~count, type = 'scatter', mode = 'markers+lines', 
             marker = list(color = viridis(6))) |>
     layout(
@@ -52,7 +64,7 @@ ui <- fluidPage(
                   max = max(df_crime_patterns$year), 
                   value = min(df_crime_patterns$year),
                   step = 1),
-      selectInput("ofnsDesc", "Crime Tyoe",
+      selectInput("ofnsDesc", "Crime Type",
                   choices = unique(df_summary$ofns_desc))
     ),
     mainPanel(
@@ -71,3 +83,5 @@ server <- function(input, output) {
       input$ofnsDesc)
   })
 }
+
+shinyApp(ui, server)
